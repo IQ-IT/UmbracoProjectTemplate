6803b6dacb42f5b51423ec9d710f0c565c871b23
Teodor Moquist <teodor@moquist.dk>
2017-01-12 13:49:08 +0100

    Fixed limit to set in cms

--- UmbracoProjectTemplate.Web/App_Plugins/maxCharacters/maxCharactersEditor.controller.js
+++ UmbracoProjectTemplate.Web/App_Plugins/maxCharacters/maxCharactersEditor.controller.js
@@ -5,6 +5,15 @@
 	editorController.$inject = ['$scope'];
 
 		function editorController($scope) {
-			$scope.limitTo = 100;
+
+			$scope.limitchars = function () {
+	      var limit = $scope.model.config.limit;
+				if ($scope.model.value.length > limit ) {
+					$scope.info = 'You cannot write more then ' + limit  + ' characters!';
+				}
+				else {
+					$scope.info = 'You have ' + (limit - $scope.model.value.length) + ' characters left.';
+				}
+	    };
 		}
 })();
