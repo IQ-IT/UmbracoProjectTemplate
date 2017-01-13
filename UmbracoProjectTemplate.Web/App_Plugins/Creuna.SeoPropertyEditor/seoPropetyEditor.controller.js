28f79456b8b21b50780d05e9cab902ca0610ae7b
Jesper Hauge <jesper.hauge@creuna.dk>
2017-01-13 14:39:17 +0100

    Stubbed SeoProperty editor

---
+++ UmbracoProjectTemplate.Web/App_Plugins/Creuna.SeoPropertyEditor/seoPropetyEditor.controller.js
@@ -0,0 +1,19 @@
+﻿/* global angular */
+
+(function() {
+    'use strict';
+
+    angular.module('umbraco').controller('Creuna.SeoPropertyController', seoPropertyController);
+    seoPropertyController.$inject = ['$scope'];
+
+    function seoPropertyController($scope) {
+        $scope.model.value = $scope.model.value ||
+        {
+            title: '',
+            description: '',
+            noIndex: false,
+            noFollow: false,
+            canonicalId: null
+        }
+    }
+})();
