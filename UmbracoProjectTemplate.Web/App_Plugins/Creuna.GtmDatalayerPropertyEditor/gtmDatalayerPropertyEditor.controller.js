1006fba1914a47adfd465da81048ef10553af282
Jesper Hauge <jesper.hauge@creuna.dk>
2017-01-16 13:22:01 +0100

    Property editor in place

---
+++ UmbracoProjectTemplate.Web/App_Plugins/Creuna.GtmDatalayerPropertyEditor/gtmDatalayerPropertyEditor.controller.js
@@ -0,0 +1,24 @@
+﻿/* global angular, _ */
+
+(function() {
+    'use strict';
+
+    angular.module('umbraco').controller('Creuna.GtmDatalayerPropertyController', editorController);
+    editorController.$inject = ['$scope'];
+
+    function editorController($scope) {
+        $scope.attrs = $scope.model.config.attributes.split(',');
+        _.each($scope.attrs, function (attr) {
+                if (!$scope.model.value.hasOwnProperty(attr))
+                    $scope.model.value[attr] = '';
+            });
+        _.each($scope.model.value, function(p) {
+                if (!$scope.model.value.hasOwnProperty(p)) return;
+                if (_.contains($scope.attrs, p)) {
+                    delete $scope.model.value[p];
+                }
+            });
+        
+        console.info($scope.model.value);
+    }
+})();
