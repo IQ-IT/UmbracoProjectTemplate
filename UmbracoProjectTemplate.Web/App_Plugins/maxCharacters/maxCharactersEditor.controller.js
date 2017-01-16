bb90128d69ea134a596f8b87906593275051f353
Teodor Moquist <teodor@moquist.dk>
2017-01-16 12:11:40 +0100

    Added 2x textboxes with maxchar

--- UmbracoProjectTemplate.Web/App_Plugins/maxCharacters/maxCharactersEditor.controller.js
+++ UmbracoProjectTemplate.Web/App_Plugins/maxCharacters/maxCharactersEditor.controller.js
@@ -6,16 +6,34 @@
     angular.module('umbraco').controller('umb.maxCharactersEditorController', editorController);
     editorController.$inject = ['$scope'];
 
-    function editorController($scope) {
+    function editorController($scope, dialogService) {
 
-        $scope.limitchars = function () {
-            var limit = $scope.model.config.limit;
-            if ($scope.model.value.length > limit) {
-                $scope.info = 'You cannot write more then ' + limit + ' characters!';
+        $scope.limitchars_title = function () {
+            var limit = $scope.model.config.limit_title;
+            if ($scope.model.title.value.length > limit) {
+                $scope.info_title = 'You cannot write more then ' + limit + ' characters!';
             }
             else {
-                $scope.info = 'You have ' + (limit - $scope.model.value.length) + ' characters left.';
+                $scope.info_title = 'You have ' + (limit - $scope.model.title.value.length) + ' characters left.';
             }
+        };
+        $scope.limitchars_description = function () {
+            var limit = $scope.model.config.limit_description;
+            if ($scope.model.description.value.length > limit) {
+                $scope.info_description = 'You cannot write more then ' + limit + ' characters!';
+            }
+            else {
+                $scope.info_description = 'You have ' + (limit - $scope.model.description.value.length) + ' characters left.';
+            }
+        };
+        $scope.pickLink = function () {
+            console.log('pick link');
+            dialogService.contentPicker({
+                multipicker: false,
+                callback: function(response) {
+                    console.log(response);
+                }
+            });
         };
     }
 })();
