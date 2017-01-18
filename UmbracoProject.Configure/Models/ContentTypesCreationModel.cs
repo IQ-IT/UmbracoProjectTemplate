db913ab9fabe19d568aa8664777a7fc2bbd40777
Jesper Hauge <jesper.hauge@creuna.dk>
2017-01-18 10:55:28 +0100

    A bit more work on configure project

--- UmbracoProject.Configure/Models/ContentTypesCreationModel.cs
+++ UmbracoProject.Configure/Models/ContentTypesCreationModel.cs
@@ -1,7 +1,14 @@
-﻿namespace UmbracoProject.Configure.Models
+﻿using Umbraco.Core.Models;
+
+namespace UmbracoProject.Configure.Models
 {
     public class ContentTypesCreationModel
     {
-        
+        public int CompositionsFolderId { get; set; }
+        public int ContainersFolderId { get; set; }
+        public int PagesFolderId { get; set; }
+        public ContentType WebsiteType { get; set; }
+        public ContentType FrontpageType { get; set; }
+        public ContentType TextpageType { get; set; }
     }
 }
